9e0064c57d62614c4613bc8cc9501af7875ff9db
unknown <thongnguyennqt@gmail.com>
2022-09-20 16:52:59 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const router = express.Router();
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+router.get("/", (req, res) => {
+  res.render("index");
+});
+
+router.get("/about", (req, res) => {
+  res.render("about", { title: "Hey", message: "Hello there!" });
+});
+
+app.use("/", router);
+app.listen(process.env.port || 3000);
+
+console.log("Running at Port 3000");
